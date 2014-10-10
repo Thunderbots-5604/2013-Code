f173aae02f7712479b37fb4188cdab6129ea4cd6
pranavmathur <pranavmathur99@gmail.com>
2014-10-10 16:11:51 -0700

    Edited includes
    Edited autoIncludes.h

--- Competition/autoIncludes.h
+++ Competition/autoIncludes.h
@@ -1,23 +1,4 @@
-#pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTMotor,  none)
-#pragma config(Hubs,  S2, HTServo,  HTMotor,  none,     none)
-#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
-#pragma config(Sensor, S2,     ,               sensorI2CMuxController)
-#pragma config(Sensor, S3,     infrared,       sensorHiTechnicIRSeeker1200)
-#pragma config(Sensor, S4,     touch,          sensorTouch)
-#pragma config(Motor,  mtr_S1_C1_1,     Left,          tmotorTetrix, openLoop, reversed, encoder)
-#pragma config(Motor,  mtr_S1_C1_2,     Right,         tmotorTetrix, openLoop, encoder)
-#pragma config(Motor,  mtr_S1_C2_1,     Lift1,         tmotorTetrix, openLoop, reversed)
-#pragma config(Motor,  mtr_S1_C2_2,     Lift2,         tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C3_1,     Left2,         tmotorTetrix, openLoop, reversed)
-#pragma config(Motor,  mtr_S1_C3_2,     Right2,        tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S2_C2_1,     Sweep,         tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S2_C2_2,     Flag,          tmotorTetrix, openLoop)
-#pragma config(Servo,  srvo_S2_C1_1,    bucket,               tServoStandard)
-#pragma config(Servo,  srvo_S2_C1_2,    pin,                  tServoStandard)
-#pragma config(Servo,  srvo_S2_C1_3,    servo3,               tServoNone)
-#pragma config(Servo,  srvo_S2_C1_4,    servo4,               tServoNone)
-#pragma config(Servo,  srvo_S2_C1_5,    servo5,               tServoNone)
-#pragma config(Servo,  srvo_S2_C1_6,    servo6,               tServoNone)
+
 
 static const float irDistances[4] = {14.5, 10, 15.5, 10};
 static float irBackDistances[4] = {irDistances[0]-7,-4,0,-3.5};
